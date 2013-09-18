9536d4b05a53cc6015157c33a06ff78cf1b9e9e2
Jussi Leinonen <jsleinonen@gmail.com>
2013-09-18 16:49:15 +0300

    Added preliminary binned PSD implementation

--- pytmatrix/tmatrix_psd.py
+++ pytmatrix/tmatrix_psd.py
@@ -79,6 +79,60 @@ class GammaPSD(object):
             return False
 
 
+class BinnedPSD(object):
+    """Binned gamma particle size distribution (PSD).
+    
+    Callable class to provide a binned PSD with the given bin edges and PSD
+    values.
+
+    Args (constructor):
+        The first argument to the constructor should specify n+1 bin edges, 
+        and the second should specify n bin_psd values.        
+        
+    Args (call):
+        D: the particle diameter.
+
+    Returns (call):
+        The PSD value for the given diameter.    
+        Returns 0 for all diameters outside the bins.
+    """
+    
+    def __init__(self, bin_edges, bin_psd):
+        if len(bin_edges) != len(bin_psd)+1:
+            raise ValueError("There must be n+1 bin edges for n bins.")
+        
+        self.bin_edges = bin_edges
+        self.bin_psd = bin_psd
+        
+    def psd_for_D(self, D):       
+        if not (self.bin_edges[0] < D <= self.bin_edges[-1]):
+            return 0.0
+        
+        # binary search for the right bin
+        start = 0
+        end = len(self.bin_edges)
+        while end-start > 1:
+            half = (start+end)//2
+            if self.bin_edges[start] < D <= self.bin_edges[half]:
+                end = half
+            else:
+                start = half
+                                
+        return bin_psd[start]                    
+        
+    def __call__(self, D):
+        if np.shape(D) == (): # D is a scalar
+            return self.psd_for_D(D)
+        else:
+            return np.array([self.psd_for_D(d) for d in D])
+    
+    def __eq__(self, other):
+        return len(self.bin_edges) == len(other.bin_edges) and \
+            (self.bin_edges == other.bin_edges).all() and \
+            (self.bin_psd == other.bin_psd).all()
+                
+                
+
 
 class TMatrixPSD(TMatrix):
     """T-matrix class to perform computations over PSDs.
