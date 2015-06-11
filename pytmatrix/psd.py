b831a25eff462a0627892af3f42597308c1bda90
Jussi Tiira <jussi.tiira@helsinki.fi>
2015-06-11 17:52:36 +0300

    check for None for python3 compatibility

--- pytmatrix/psd.py
+++ pytmatrix/psd.py
@@ -240,6 +240,10 @@ class BinnedPSD(PSD):
             return np.array([self.psd_for_D(d) for d in D])
     
     def __eq__(self, other):
+        if self is None or other is None:
+            if self is None and other is None:
+                return True
+            return False
         return len(self.bin_edges) == len(other.bin_edges) and \
             (self.bin_edges == other.bin_edges).all() and \
             (self.bin_psd == other.bin_psd).all()
